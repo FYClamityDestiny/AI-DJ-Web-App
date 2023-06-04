d301a575a9c849cd21d49972ed8827ff52ab2684
FYClamityDestiny <99613329+FYClamityDestiny@users.noreply.github.com>
2023-06-04 10:10:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-function setup(){
-canvas = createCanvas(600,500);
-canvas.center();
+songpp = '';
+songpotter = '';
+function preload() {
+    songpp = loadSound("music2.mp3");
+    songpotter = loadSound("music.mp3");
+}
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
 }
