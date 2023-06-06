2c9ca86d51b953357c9fe762766ca9f1be7e283e
FYClamityDestiny <99613329+FYClamityDestiny@users.noreply.github.com>
2023-06-06 14:44:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
 songpp = '';
 songpotter = '';
 function preload() {
@@ -9,7 +13,25 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video,modelLoaded)
+    poseNet.on();
 }
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+function modelLoaded(){
+console.log(ml5.version + "Posenet Initalized");
+}
+function gotPoses(error,results){
+if(error){
+console.error();
+}
+else{
+if(results.length > 0){
+leftWristX = results[0].pose.leftWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+rightWristX = results[0].pose.rightWrist.x;
+rightWristY = results[0].pose.rightWrist.y;
+}
+}
 }
